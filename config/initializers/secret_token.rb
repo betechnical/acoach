996606c94244730ee1b41ca49b74e671d87522e9
Cotille Davis <cotille@gmail.com>
2011-11-29 19:27:15 -0800

    new rails app for acoach project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acoach::Application.config.secret_token = '2128fc9f3659d3727d6dbb5ab44cbb5548da21981982e69f4a2757e561b489ceb6af255976924de6ad866e7ab7213678b4bc6b0c5c404f41b605e0b32192302e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Acoach::Application.config.session_store :cookie_store, key: '_acoach_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Acoach::Application.config.session_store :active_record_store
