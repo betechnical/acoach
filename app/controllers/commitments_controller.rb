a7c6032aec15079dff3ea8c9fe7a2df1384454f0
Cotille Davis <cotille@gmail.com>
2011-12-03 14:57:27 -0800

    added a list page

--- app/controllers/commitments_controller.rb
+++ app/controllers/commitments_controller.rb
@@ -1,6 +1,9 @@
 class CommitmentsController < ActionController::Base
 	def create
 		Commitment.create params[:commitment]
-		redirect_to "/"
+		redirect_to commitments_path
+	end
+	def index
+		@commitments=Commitment.all
 	end
 end
