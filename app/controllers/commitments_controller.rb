9ae0dd722bf5dac4ceb6ffc85da3f7c25f61bfc5
Sarah Seegal <sarahseegal@Sarah-Seegals-MacBook-Air.local>
2011-12-03 17:27:36 -0800

    added new page, header and general styling

--- app/controllers/commitments_controller.rb
+++ app/controllers/commitments_controller.rb
@@ -1,9 +1,11 @@
-class CommitmentsController < ActionController::Base
+class CommitmentsController < ApplicationController
 	def create
 		Commitment.create params[:commitment]
 		redirect_to commitments_path
 	end
 	def index
 		@commitments=Commitment.all
+	end
+	def new
 	end
 end
