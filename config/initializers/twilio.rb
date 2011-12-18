08f3315c1f9fa6b3582fc7cc5fa35c0f97718849
Sarah Seegal <sarahseegal@Sarah-Seegals-MacBook-Air.local>
2011-12-17 16:24:03 -0800

    added twilio and sms functionality

---
+++ config/initializers/twilio.rb
@@ -0,0 +1,11 @@
+account_sid = 'AC20dd9422ed354694837a9a8a97bc6cd0'
+auth_token = '50f3b23319969773d494e5faebb3963f'
+
+$TwilioClient = Twilio::REST::Client.new account_sid, auth_token
+
+# send an sms
+$TwilioClient.account.sms.messages.create(
+  :from => '+14157893510',
+  :to => '+14157228354',
+  :body => "Hey there danger sparkle. What's cooking good looking!"
+)
